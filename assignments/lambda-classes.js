32516a7c7d985737a73d8dcf6470fd603507e567
Tommy <tommykindle83@gmail.com>
2019-06-13 12:50:13 -0700

    Instructor class created for classes assignment

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,3 +9,20 @@ class Person {
         `Hello my name is ${this.name}, I am from ${this.location} `
     }
 }
+
+class Instructor extends Person {
+    constructor(props) {
+        super(props);
+        this.specialty = props.specialty
+        this.favLanguage = props.favLanguage
+        this.catchPhrase = props.catchPhrase
+    }
+    demo(subject) {
+        `Today we are learning about ${subject}`
+    }
+
+    grade(student, subject) {
+        `${student.name} receives a perfect score on ${subject}`
+    }
+}
+
