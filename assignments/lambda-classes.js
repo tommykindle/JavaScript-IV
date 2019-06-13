6e521b3212f52ac4d52966fc8c00f84c459e1c0b
Tommy <tommykindle83@gmail.com>
2019-06-13 12:30:38 -0700

    Person class created for classes assignment

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(props) {
+        this.name = props.name
+        this.age = props.age
+        this.location = props.location
+    }
+    speak() {
+        `Hello my name is ${this.name}, I am from ${this.location} `
+    }
+}
